d579fc84c9c59708b02172f83708ce57f3cfdfca
Gabriel Kist Camargo <gabrielkcamargo0@hotmail.com>
2023-09-08 22:33:12 -0300

    Added - Novas funcionalidades

--- main.js
+++ main.js
@@ -1,89 +1,88 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const calcularButton = document.getElementById("calcular");
-    const resultadoRotulo = document.getElementById("resultado-rotulo");
-    const resultadoAlca = document.getElementById("resultado-alca");
-    const resultadoEmbaladora = document.getElementById("resultado-embaladora");
-    const resultadoPaletizadora = document.getElementById("resultado-paletizadora");
+  const calcularButton = document.getElementById("calcular");
+  const imprimirButton = document.getElementById("imprimir");
+  const resultadoRotulo = document.getElementById("resultado-rotulo");
+  const resultadoAlca = document.getElementById("resultado-alca");
+  const resultadoEmbaladora = document.getElementById("resultado-embaladora");
+  const resultadoPaletizadora = document.getElementById(
+    "resultado-paletizadora"
+  );
 
-    calcularButton.addEventListener("click", function () {
-        const quantiaRotuloInput = document.getElementById("rotulo");
-        const quantiaAlcaInput = document.getElementById("alca");
-        const quantiaPlasticoEmbaladoraInput = document.getElementById("plastico-embaladora");
-        const quantiaPlasticoPaletizadoraInput = document.getElementById("plastico-paletizadora");
+  calcularButton.addEventListener("click", function () {
+    const quantiaRotuloInput = document.getElementById("rotulo");
+    const quantiaAlcaInput = document.getElementById("alca");
+    const quantiaPlasticoEmbaladoraInput = document.getElementById(
+      "plastico-embaladora"
+    );
+    const quantiaPlasticoPaletizadoraInput = document.getElementById(
+      "plastico-paletizadora"
+    );
 
-        const quantiaRotulo = parseInt(quantiaRotuloInput.value);
-        const quantiaAlca = parseInt(quantiaAlcaInput.value);
-        const quantiaPlasticoEmbaladora = parseFloat(quantiaPlasticoEmbaladoraInput.value);
-        const quantiaPlasticoPaletizadora = parseFloat(quantiaPlasticoPaletizadoraInput.value);
+    const quantiaRotulo = parseInt(quantiaRotuloInput.value);
+    const quantiaAlca = parseInt(quantiaAlcaInput.value);
+    const quantiaPlasticoEmbaladora = parseFloat(
+      quantiaPlasticoEmbaladoraInput.value
+    );
+    const quantiaPlasticoPaletizadora = parseFloat(
+      quantiaPlasticoPaletizadoraInput.value
+    );
 
-        // Verifique se algum dos campos está vazio
-        if (
-            isNaN(quantiaRotulo) ||
-            isNaN(quantiaAlca) ||
-            isNaN(quantiaPlasticoEmbaladora) ||
-            isNaN(quantiaPlasticoPaletizadora)
-        ) {
-            alert("Por favor, preencha todos os campos com valores numéricos.");
-            return;
-        }
+    // Verifique se algum dos campos está vazio
+    if (
+      isNaN(quantiaRotulo) ||
+      isNaN(quantiaAlca) ||
+      isNaN(quantiaPlasticoEmbaladora) ||
+      isNaN(quantiaPlasticoPaletizadora)
+    ) {
+      alert("Por favor, preencha todos os campos com valores numéricos.");
+      return;
+    }
 
-        const resultadoRotuloValue = quantiaRotulo * 2500;
-        const resultadoAlcaValue = quantiaAlca * 7143;
-        const resultadoEmbaladoraValue = quantiaPlasticoEmbaladora * 27.07;
-        const resultadoPaletizadoraValue = quantiaPlasticoPaletizadora * 16.91;
+    const resultadoRotuloValue = quantiaRotulo * 2500;
+    const resultadoAlcaValue = quantiaAlca * 7143;
+    const resultadoEmbaladoraValue = quantiaPlasticoEmbaladora * 27.07;
+    const resultadoPaletizadoraValue = quantiaPlasticoPaletizadora * 16.91;
 
-        resultadoRotulo.textContent = `Rótulo: ${resultadoRotuloValue}`;
-        resultadoAlca.textContent = `Alça: ${resultadoAlcaValue}`;
-        resultadoEmbaladora.textContent = `Plástico Embaladora: ${resultadoEmbaladoraValue}`;
-        resultadoPaletizadora.textContent = `Plástico Paletizadora: ${resultadoPaletizadoraValue}`;
-    });
-});
+    resultadoRotulo.textContent = `Rótulo: ${resultadoRotuloValue}`;
+    resultadoAlca.textContent = `Alça: ${resultadoAlcaValue}`;
+    resultadoEmbaladora.textContent = `Plástico Embaladora: ${resultadoEmbaladoraValue}`;
+    resultadoPaletizadora.textContent = `Plástico Paletizadora: ${resultadoPaletizadoraValue}`;
 
+    // Exibir uma caixa de diálogo de confirmação
+    const imprimirResultados = confirm("Deseja imprimir os resultados?");
 
-document.addEventListener("DOMContentLoaded", function () {
-    const calcularDiferencaButton = document.getElementById("calcular-diferenca");
-    const dataInicialInput = document.getElementById("data-inicial");
-    const dataFinalInput = document.getElementById("data-final");
-    const resultadoDiferenca = document.getElementById("resultado-diferenca");
+    if (imprimirResultados) {
+      // Definir o nome do arquivo ao salvar a impressão
 
-    calcularDiferencaButton.addEventListener("click", function () {
-        const dataInicial = new Date(dataInicialInput.value);
-        const dataFinal = new Date(dataFinalInput.value);
+      const nomeDoArquivo = `${getDataFormatada()}_linha_5_material`;
+      function getDataFormatada() {
+        const dataAtual = new Date();
+        const dia = String(dataAtual.getDate()).padStart(2, "0");
+        const mes = String(dataAtual.getMonth() + 1).padStart(2, "0"); // Lembre-se de que os meses são base 0 (janeiro é 0)
+        const ano = dataAtual.getFullYear();
+        return `${dia}/${mes}/${ano}`;
+      }
 
-        if (!isNaN(dataInicial.getTime()) && !isNaN(dataFinal.getTime())) {
-            if (dataFinal >= dataInicial) {
-                const umDiaEmMilissegundos = 24 * 60 * 60 * 1000; // Um dia em milissegundos
-                const diferencaEmMilissegundos = dataFinal - dataInicial;
-                const diferencaEmDias = Math.floor(diferencaEmMilissegundos / umDiaEmMilissegundos);
-
-                resultadoDiferenca.textContent = `Diferença em dias: ${diferencaEmDias} dias`;
-            } else {
-                resultadoDiferenca.textContent = "A data final não pode ser anterior à data inicial.";
-            }
-        } else {
-            resultadoDiferenca.textContent = "Por favor, insira datas válidas.";
-        }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const menuLink = document.getElementById("menu-link");
-
-    menuToggle.addEventListener("click", function () {
-        const menuHamburger = menuToggle.parentElement;
-        if (menuHamburger.classList.contains("menu-open")) {
-            menuHamburger.classList.remove("menu-open");
-        } else {
-            menuHamburger.classList.add("menu-open");
-        }
-    });
-
-    // Fechar o menu se o usuário clicar em qualquer lugar fora dele
-    document.addEventListener("click", function (event) {
-        if (!menuToggle.contains(event.target) && !menuLink.contains(event.target)) {
-            const menuHamburger = menuToggle.parentElement;
-            menuHamburger.classList.remove("menu-open");
-        }
-    });
+      // Abra a caixa de diálogo de impressão do navegador
+      const win = window.open("", "_blank");
+      win.document.write(`
+                <html>
+                <head>
+                    <title>${nomeDoArquivo}</title>
+                </head>
+                <body>
+                    <pre>
+                        Rótulo: ${resultadoRotuloValue}
+                        Alça: ${resultadoAlcaValue}
+                        Plástico Embaladora: ${resultadoEmbaladoraValue}
+                        Plástico Paletizadora: ${resultadoPaletizadoraValue}
+                    </pre>
+                </body>
+                </html>
+            `);
+      win.document.close();
+      win.print();
+      win.close();
+    }
+  });
 });
